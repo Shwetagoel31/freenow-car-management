fb0ff14ac900ddf37ef62a9a55b26078b891fa64
Shweta Goel <shwetagoel31@gmail.com>
2020-10-27 16:00:40 +0530

    JIRA9999 Added SecurityConfig

--- src/main/java/com/freenow/carmgmt/config/SecurityConfig.java
+++ src/main/java/com/freenow/carmgmt/config/SecurityConfig.java
@@ -1,24 +1,54 @@
 package com.freenow.carmgmt.config;
 
-/*@Configuration
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception
     {
-        auth.inMemoryAuthentication()
+        auth
+            .inMemoryAuthentication()
             .withUser("foo")
             .password("bar")
-            .roles("USER");
+            .roles("ADMIN");
     }
-    
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
-        
-        
+        //@formatter:off
+
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/login", "h2-console/***")
+                    .permitAll()
+                .antMatchers("/api/v1/drivers")
+                    .hasAnyRole("ADMIN", "DRIVER")
+                .antMatchers("/api/v1/cars")
+                    .hasRole("ADMIN")
+                .anyRequest().authenticated();
+        /* .and()
+         .formLogin()
+             .defaultSuccessUrl("/swagger-ui.html");*/
+
+        //@formatter:on
+
     }
     
-    
-}*/
+    @Bean
+    public PasswordEncoder BCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
